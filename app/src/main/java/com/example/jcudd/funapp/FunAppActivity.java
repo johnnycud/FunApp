050ea961b20ce864c042140337828002756786c5
jcuddles@msn.com <jcuddles@msn.com>
2018-04-25 11:30:44 +0100

    First Commit

---
+++ app/src/main/java/com/example/jcudd/funapp/FunAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.jcudd.funapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FunAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_app);
+    }
+}
